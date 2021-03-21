f5ea45cc9ce50341ed7f859ca5b9c0a48e9de223
unknown <rizdaagisa@gmail.com>
2021-03-21 11:51:40 +0700

    botum

--- odb/pypyodbc.py
+++ odb/pypyodbc.py
@@ -433,7 +433,7 @@ else:
         if library is None:
             # If find_library still can not find the library
             # we try finding it manually from where libodbc.so usually appears
-            lib_paths = ("/usr/lib/libodbc.so","/usr/lib/i386-linux-gnu/libodbc.so","/usr/lib/x86_64-linux-gnu/libodbc.so","/usr/lib/libiodbc.dylib")
+            lib_paths = ("/usr/lib/libodbc.so","/usr/lib/i386-linux-gnu/libodbc.so","/usr/lib/x86_64-linux-gnu/libodbc.so","/usr/lib/libiodbc.dylib","/usr/lib/snowflake/odbc/lib/libSnowflake.so")
             lib_paths = [path for path in lib_paths if os.path.exists(path)]
             if len(lib_paths) == 0 :
                 raise OdbcNoLibrary('ODBC Library is not found. Is LD_LIBRARY_PATH set?')
